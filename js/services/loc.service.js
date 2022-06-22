80074baa920760373823bb1dedb3602c35522674
eran_pascal <eranpascal@gmail.com>
2022-06-22 20:21:31 +0300

    exportlocs-updated

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,35 +2,18 @@
 import {utilService} from "/js/services/util.service.js";
 
 export const locService = {
-<<<<<<< HEAD
-  getLocs,
-};
-=======
     getLocs,
     createLocation,
     updateLocs,
 }
 
->>>>>>> 23056c51475d15b12a016f2c812d7a26addf4c35
 
 
 
 const locs = [
-<<<<<<< HEAD
   { name: "Greatplace", lat: 32.047104, lng: 34.832384 },
   { name: "Neveragain", lat: 32.047201, lng: 34.832581 },
-  // id: getId(),
-  // name: onNameSubmint(),
-  // lat: lat,
-  // lng: lng,
-  // createdAt,
-  // updatedAt,
 ];
-=======
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
->>>>>>> 23056c51475d15b12a016f2c812d7a26addf4c35
 
 function getLocs() {
   return new Promise((resolve, reject) => {
@@ -39,8 +22,6 @@ function getLocs() {
     }, 2000);
   });
 }
-<<<<<<< HEAD
-=======
 
 function createLocation(name, lat, lng) {
     return {
@@ -54,18 +35,10 @@ function createLocation(name, lat, lng) {
 }
 
 
-// function makeId(length = 2) {
-//     var txt = '' 
-//     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-//     for (var i = 0; i < length; i++) {
-//         txt += possible.charAt(Math.floor(Math.random() * possible.length))
-//     }
-//     return txt
-// } 
+
 
 
 function updateLocs(location){
     console.log('updating locs', location);
     locs.push(location)
 }
->>>>>>> 23056c51475d15b12a016f2c812d7a26addf4c35
