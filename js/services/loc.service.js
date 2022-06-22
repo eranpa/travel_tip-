23056c51475d15b12a016f2c812d7a26addf4c35
eran_pascal <eranpascal@gmail.com>
2022-06-22 20:15:30 +0300

    added onsumbit function

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,10 +1,17 @@
+
+import {utilService} from "/js/services/util.service.js";
+
 export const locService = {
-    getLocs
+    getLocs,
+    createLocation,
+    updateLocs,
 }
 
 
+
+
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
@@ -16,4 +23,29 @@ function getLocs() {
     });
 }
 
+function createLocation(name, lat, lng) {
+    return {
+        id: utilService.makeId(4),
+        name,
+        lat,
+        lng,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+    }
+}
 
+
+// function makeId(length = 2) {
+//     var txt = '' 
+//     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+//     for (var i = 0; i < length; i++) {
+//         txt += possible.charAt(Math.floor(Math.random() * possible.length))
+//     }
+//     return txt
+// } 
+
+
+function updateLocs(location){
+    console.log('updating locs', location);
+    locs.push(location)
+}
